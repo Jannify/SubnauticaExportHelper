d6e63421e0b111341968ffbbfc320a2cc56093e5
Jannify <23176718+Jannify@users.noreply.github.com>
2022-03-15 19:03:48 +0100

    Fixed wrong paths

--- SubnauticaExportHelper/PostExporter/FixSubnauticaAssetsPostExporter.cs
+++ SubnauticaExportHelper/PostExporter/FixSubnauticaAssetsPostExporter.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.IO;
 using AssetRipper.Core.Logging;
 using AssetRipper.Library;
@@ -26,16 +26,16 @@ public class FixSubnauticaAssetsPostExporter : IPostExporter
         FixReplaceCode(Path.Combine(assetsPath, "Resources", "uGUI.prefab"), ScriptFiles.CameraScannerRoomPattern, ScriptFiles.CameraScannerRoomReplacement);
 
         Info("Moving GeneratorPieces AssetBundle");
-        string basePiecesDir = Path.Combine(assetsPath, "Prefabs", "Base", "GeneratorPieces");
+        string basePiecesDir = Path.Combine(assetsPath, "Prefabs", "Base");
         Directory.CreateDirectory(basePiecesDir);
-        Directory.Move(Path.Combine(assetsPath, "Asset_Bundles", "basegeneratorpieces", "prefabs", "base", "generatorpieces"), Path.Combine(basePiecesDir));
+        Directory.Move(Path.Combine(assetsPath, "Asset_Bundles", "basegeneratorpieces", "prefabs", "base", "generatorpieces"), Path.Combine(basePiecesDir, "GeneratorPieces"));
 
         Info("Moving Logo AssetBundle");
         Directory.Move(Path.Combine(assetsPath, "Asset_Bundles", "logos"), Path.Combine(assetsPath, "Logos"));
 
         Info("Adding AssetBundle tag");
         File.WriteAllText(Path.Combine(basePiecesDir, "GeneratorPieces.meta"), ScriptFiles.AssetBundleMeta.Replace("{1}", RandomGuid()).Replace("{2}", "basegeneratorpieces"));
-        File.WriteAllText(Path.Combine(assetsPath, "Logos", "Logos.meta"), ScriptFiles.AssetBundleMeta.Replace("{1}", RandomGuid()).Replace("{2}", "logos"));
+        File.WriteAllText(Path.Combine(assetsPath, "Logos.meta"), ScriptFiles.AssetBundleMeta.Replace("{1}", RandomGuid()).Replace("{2}", "logos"));
         File.WriteAllText(Path.Combine(assetsPath, "Asset_Bundles", "waterdisplacement.meta"), ScriptFiles.AssetBundleMeta.Replace("{1}", RandomGuid()).Replace("{2}", "waterdisplacement"));
     }
 
